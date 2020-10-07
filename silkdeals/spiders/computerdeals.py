dd14839a325a5ab7b06801c2faa0bb9f0e252cd7
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-10-08 00:36:43 +0200

    Handle the pagination

--- silkdeals/spiders/computerdeals.py
+++ silkdeals/spiders/computerdeals.py
@@ -25,3 +25,12 @@ class ComputerdealsSpider(scrapy.Spider):
                 "store_name": product.xpath(".//button[@class='itemStore bp-p-storeLink bp-c-linkableButton bp-c-button--link  bp-c-button']/text()").get(),
                 "price": product.xpath("normalize-space(.//div[@class='itemPrice  wide ']/text())").get(),
             }
+
+        next_page = response.xpath("//a[@data-role='next-page']/@href").get()
+        if next_page:
+            absolute_url = f"https://slickdeals.net{next_page}"
+            yield SeleniumRequest(
+                url=absolute_url,
+                wait_time=3,
+                callback=self.parse
+            )
